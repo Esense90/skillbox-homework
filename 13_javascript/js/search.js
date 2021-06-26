952fed119e99cd32846e45f9cceb86793ce222ed
Kirill <kirillryabushev@gmail.com>
2021-06-27 10:49:03 +1100

    hw13 add src

--- 13_javascript/js/search.js
+++ 13_javascript/js/search.js
@@ -1,12 +1,17 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header__search-btn').addEventListener('click', function() {
-      document.querySelector('.search__input').classList.toggle('active')
-  })
-  document.querySelector('.header__search-btn').addEventListener('click', function() {
+let search__activeStatus = false
+
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.header__search-btn').addEventListener('click', function () {
+    if (search__activeStatus) return
+    search__activeStatus = true
+    document.querySelector('.search__input').classList.toggle('active')
     document.querySelector('.closed__search-btn').classList.toggle('active')
+    document.querySelector('.search__input').focus()
   })
-  document.querySelector('.closed__search-btn').addEventListener('click', function() {
+  document.querySelector('.closed__search-btn').addEventListener('click', function () {
+    search__activeStatus = false
     document.querySelector('.search__input').classList.remove('active')
     document.querySelector('.closed__search-btn').classList.remove('active')
+    document.querySelector('.search__input').value = ""
   })
 })

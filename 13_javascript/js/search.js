3b38aafd78085c6d220d3e7e93746b9b7d0c734a
Kirill <kirillryabushev@gmail.com>
2021-06-26 17:20:18 +1100

    hw13 add searsh

---
+++ 13_javascript/js/search.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.header__search-btn').addEventListener('click', function() {
+      document.querySelector('.search__input').classList.toggle('active')
+  })
+})

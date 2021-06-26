f7aee43e4e4e42548c95ae3b96c483a28cb5e80b
Kirill <kirillryabushev@gmail.com>
2021-06-27 09:45:04 +1100

    hw13 add search

--- 13_javascript/js/search.js
+++ 13_javascript/js/search.js
@@ -2,4 +2,11 @@ window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.header__search-btn').addEventListener('click', function() {
       document.querySelector('.search__input').classList.toggle('active')
   })
+  document.querySelector('.header__search-btn').addEventListener('click', function() {
+    document.querySelector('.closed__search-btn').classList.toggle('active')
+  })
+  document.querySelector('.closed__search-btn').addEventListener('click', function() {
+    document.querySelector('.search__input').classList.remove('active')
+    document.querySelector('.closed__search-btn').classList.remove('active')
+  })
 })

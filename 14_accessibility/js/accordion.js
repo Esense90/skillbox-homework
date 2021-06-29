46fde8910d77edc2074cc945a3a2b126f2358301
Kirill <kirillryabushev@gmail.com>
2021-06-29 13:06:34 +1100

    home work 14 cancel

---
+++ 14_accessibility/js/accordion.js
@@ -0,0 +1,12 @@
+  $( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true,
+      header: "h3",
+      heightStyle: "content",
+      active: "none",
+      animate: 400,
+      icons: false,
+    });
+  });
+
+
